b11b05ea5fbaed598af3aeb3bec9e0f52e5a9a55
aayman01 <ayman.alfi098@gmail.com>
2024-07-04 19:02:48 +0600

    education section complete

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -1,17 +1,13 @@
 import SectionTItle from "../../Shared/SectionTItle";
 import "./Education.css";
+import education from '../../assets/education.png'
+import nohat from '../../assets/nohat.png'
+import certificate from '../../assets/certificate.png'
 const Education = () => {
-  const timelineItems = [
-    { id: 1, content: "Started project", date: "January 2023" },
-    { id: 2, content: "First milestone", date: "February 2023" },
-    { id: 3, content: "Beta release", date: "March 2023" },
-    { id: 4, content: "Official launch", date: "April 2023" },
-  ];
-
   return (
     <div className="container">
       <SectionTItle heading="Educations"></SectionTItle>
-      <div className="timeline flex flex-col">
+      {/* <div className="timeline flex md:flex-col ">
         <div className="checkpoint">
           <div className="bg-primary">
             <h3 className="text-xl font-bold">2019-2020</h3>
@@ -33,6 +29,43 @@ const Education = () => {
         <div className="checkpoint">
           <div className="bg-primary">
             <h3 className="font-bold text-xl">2024</h3>
+            <h2 className="text-xl font-bold">Web Development</h2>
+            <p className="font-bold">Programming hero</p>
+            <p className="font-medium">Completed</p>
+          </div>
+        </div>
+      </div> */}
+      <div className="flex flex-col md:flex-row items-center justify-center gap-6 pb-4">
+        <div className="card bg-base-100 shadow-xl w-[300px]">
+          <figure className="pt-2">
+            <img className="w-24" src={education} alt="icon" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">2019-2020</h2>
+            <h2 className="font-bold text-xl">SSC</h2>
+            <p className="text-lg font-bold">Khulna Zilla School</p>
+            <p className="font-medium">GPA - 4.56 out of 5</p>
+          </div>
+        </div>
+        <div className="card bg-base-100 shadow-xl w-[300px]">
+          <figure className="pt-2">
+            <img className="w-24" src={nohat} alt="icon" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">2019-2020</h2>
+            <h2 className="text-xl font-bold">Diploma in Civil</h2>
+            <p className="text-lg font-bold">Khulna Polytechnic Institute</p>
+            <p className="font-medium">
+              Currently Studying(Will be Graduate in 2025)
+            </p>
+          </div>
+        </div>
+        <div className="card bg-base-100 shadow-xl w-[300px]">
+          <figure className="pt-2">
+            <img className="w-24" src={certificate} alt="icon" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">2019-2020</h2>
             <h2 className="text-xl font-bold">Web Development</h2>
             <p className="font-bold">Programming hero</p>
             <p className="font-medium">Completed</p>
